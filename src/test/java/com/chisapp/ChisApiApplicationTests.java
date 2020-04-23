ded9aaa8fd54306d4ea7eee6f10af076564fbe1c
Tandy <55312153@qq.com>
2020-04-23 17:03:03 +0800

    财务报表--销售统计--去除退货数据

--- src/test/java/com/chisapp/ChisApiApplicationTests.java
+++ src/test/java/com/chisapp/ChisApiApplicationTests.java
@@ -5,23 +5,17 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.math.BigDecimal;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ChisApiApplicationTests {
 
     @Test
     public void contextLoads() {
-        System.out.println("hello");
     }
 
 
     @Test
     public void test1() {
-        BigDecimal b = new BigDecimal("2");
-        int i = 10;
-        System.out.println(b.add(BigDecimal.valueOf(i)));
     }
 
 }
